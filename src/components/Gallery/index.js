cef81d9c221f870abfceee35401435b5099a6ea4
Schaffer <mr.giancarloschaffer@gmail.com>
2021-04-18 10:51:10 -0300

    Troca componente gallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ImageGallery from "react-image-gallery";
 import { useMediaQuery } from "react-responsive";
+
+import Carousel from "react-gallery-carousel";
+import "react-gallery-carousel/dist/index.css";
+
 import "./style.scss";
 
 function importAll(r) {
@@ -22,13 +26,21 @@ const Gallery = () => {
     original: i.default,
     thumbnail: i.default,
   }));
+
+  const photos = images.map((i) => ({
+    src: i.default,
+  }));
+
   return (
-    <ImageGallery
-      items={items}
-      showNav={false}
-      showPlayButton={false}
-      lazyLoad={true}
-      thumbnailPosition={isMobile ? "bottom" : "right"}
+    <Carousel
+      className="gallery-photo"
+      images={photos}
+      hasLeftButton={false}
+      hasRightButton={false}
+      hasMediaButton={false}
+      hasIndexBoard={false}
+      hasSizeButton="bottomRight"
+      objectFit="scale-down"
     />
   );
 };
